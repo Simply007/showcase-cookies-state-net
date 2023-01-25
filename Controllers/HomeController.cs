49d58cfa1e99d2ba6f61724d4db852f141eaa5f4
Ondřej Chrastina <ondrejch@kontent.ai>
2023-01-25 12:32:28 +0100

    Revert "showcase dynamic access for response"
    This reverts commit 3e52cf9913aeb1d6f522783fdc082e3a63c3338d.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Diagnostics;
 using Kontent.Ai.Delivery.Abstractions;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Linq;
 using ShowcaseCookiesStateNet.Models;
 using ShowcaseCookiesStateNet.Models.Generated;
 
@@ -24,15 +23,14 @@ public class HomeController : Controller
     public async Task<IActionResult> Index()
     {
         var client = (Request.Cookies["preview"] ?? "false") == "false" ? _productionDeliveryClient : _previewDeliveryClient;
-        var home = await client.GetItemAsync<object>("homepage");
+        var home = await client.GetItemAsync<Homepage>("homepage");
 
-        var result = JObject.Parse(home.ApiResponse.Content);
+        var title = home.Item.Content
+            .OfType<Message>()
+            .FirstOrDefault()
+            ?.Title;
 
-
-        var contentItemCodename = result["item"]?["elements"]?["content"]?["value"]?.First().ToString();
-        var title = result?["modular_content"]?[contentItemCodename ?? ""]?["elements"]?["title"]?["value"]?.ToString();
-
-        ViewBag.Title = title ?? "TITLE NOT FOUND - check implementation";
+        ViewBag.Title = title;
 
         return View();
     }
